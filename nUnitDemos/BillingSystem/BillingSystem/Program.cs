4ca422efcc814cd60c1fd94f0a2478f4f2f05126
TechieSyed <syed.khaleel@outlook.com>
2022-01-18 13:10:45 +0530

    Moq

---
+++ nUnitDemos/BillingSystem/BillingSystem/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillingSystem
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+    }
+    public class Customer
+    {
+        public string Email { get; set; }
+    }
+    public class InvoiceManager
+    {
+        IEmailSender emailSender;
+        public InvoiceManager(IEmailSender emailSender)
+        {
+            this.emailSender = emailSender;
+        }
+        public bool ConfirmInvoice(Invoice invoice, Customer customer)
+        {
+            Console.WriteLine("Invoice confirmed : {0}", invoice.InvoiceId);
+            emailSender.SendEmail(customer.Email, "Your invoice confirmed. ID = " + invoice.InvoiceId);
+            return  emailSender.SendEmail(customer.Email, "Your invoice confirmed. ID = " + invoice.InvoiceId);
+           // return true;
+        }
+    }
+
+    public interface IEmailSender
+    {
+        bool SendEmail(string toAddress, string message);
+    }
+    public class GmailSender : IEmailSender
+    {
+        public bool SendEmail(string toAddress, string message)
+        {
+            Console.WriteLine("Sending email to {0}\nMessage : {1}", toAddress, message);
+            return true;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            var invoice = new Invoice { InvoiceId = 101 };
+            var customer = new Customer { Email = "techiesyed@outlook.com" };
+
+            IEmailSender emailSender = new GmailSender();
+            var invoiceManager = new InvoiceManager(emailSender);
+            bool result = invoiceManager.ConfirmInvoice(invoice, customer);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ nUnitDemos/BillingSystem/BillingSystemTests/InvoiceManagerShould.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using Moq;
+using BillingSystem;
+namespace BillingSystemTests
+{
+    [TestFixture]
+    public class InvoiceManagerShould
+    {
+        private InvoiceManager SUT;
+        private Mock<IEmailSender> EmailSenderMock;
+        [SetUp]
+        public void Init()
+        {
+            EmailSenderMock = new Mock<IEmailSender>();
+        }
+
+        [Test]
+        public void Confirm_Invoice()
+        {
+            SUT = new InvoiceManager(EmailSenderMock.Object);
+
+            var invoice = new Invoice { InvoiceId = 101 };
+            var customer = new Customer { Email = "techiesyed@outlook.com" };
+
+            var actual = SUT.ConfirmInvoice(invoice, customer);
+            Assert.That(actual, Is.True);
+        }
+
+        [Test]
+        public void Confirm_Invoice_Using_String_Email()
+        {
+            EmailSenderMock.Setup(m => m.SendEmail(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+            SUT = new InvoiceManager(EmailSenderMock.Object);
+
+            var invoice = new Invoice { InvoiceId = 101 };
+            var customer = new Customer { Email = "techiesyed@outlook.com" };
+
+            var actual = SUT.ConfirmInvoice(invoice, customer);
+            Assert.That(actual, Is.True);
+        }
+
+        [Test]
+        public void Confirm_Invoice_Calls_SendEmail_Only_Once()
+        {
+            EmailSenderMock.Setup(m => m.SendEmail(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+            SUT = new InvoiceManager(EmailSenderMock.Object);
+
+            var invoice = new Invoice { InvoiceId = 101 };
+            var customer = new Customer { Email = "techiesyed@outlook.com" };
+
+            var actual = SUT.ConfirmInvoice(invoice, customer);
+            EmailSenderMock.Verify(m => m.SendEmail(It.IsAny<string>(), It.IsAny<string>()), Times.Exactly(1));
+            Assert.That(actual, Is.True);
+        }
+    }
+}
