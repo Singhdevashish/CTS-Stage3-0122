9045f3f2f4e8c5db5059eb4250ee1b95a96433eb
TechieSyed <syed.khaleel@outlook.com>
2022-01-24 16:30:59 +0530

    MVC Crud Fixed

---
+++ DotNetCore/Mvc_CRUD/Mvc_CRUD/Controllers/ProductsController.cs
@@ -0,0 +1,95 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Mvc_CRUD.Models;
+namespace Mvc_CRUD.Controllers
+{
+    public class ProductsController : Controller
+    {
+        //private readonly ProductsDbContext context = new ProductsDbContext()
+        private readonly ProductsDbContext context;
+        public ProductsController(ProductsDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var queryProducts = from p in context.Products
+                                select p;
+            var products = queryProducts.ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+                return View(product);
+
+            context.Products.Add(product);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = context.Products.Find(id);
+
+            if (product == null) return NotFound();
+
+            return View(product);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var product = context.Products.Find(id);
+
+            if (product == null) return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (!ModelState.IsValid)
+                return View(product);
+
+            context.Attach(product);
+            context.Entry(product).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        
+        public IActionResult Delete(int id)
+        {
+            var product = context.Products.Find(id);
+
+            if (product == null) return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var product = context.Products.Find(id);
+            context.Products.Remove(product);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
